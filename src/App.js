98b5dab503e3d1ab46000a80611e2bd37e7fef58
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-02 16:21:14 +0500

    added app function using state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
+import { PhotoshopPicker } from "react-color"
+
+
+export default function App() {
+  const [color, setColor] = useState("#ff0000");
+  return (
+    <div>
+      <PhotoshopPicker
+        color={color}
+        onChangeComplete={color => {
+          setColor(color.hex);
+        }}
+      />
+      <div
+        style={{
+          backgroundColor: color,
+          height: "300px",
+          transition: "ease all 500ms"
+        }}
+      />
+    </div>
+  );
+}
 
